1ca8e7199569ffc280afba4bc5f7e2d22cea0e6b
Paul Ybarra <ybarrap@earthlink.net>
2018-10-24 19:13:06 -0500

    Added Div Component

--- components/counter.jsx
+++ components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     render() { 
-        return (<h1>Hello World</h1>  );
+        return <div><h1>Hello World</h1></div>;
     }
 }
- <h1>Hello World</h1>
+ 
